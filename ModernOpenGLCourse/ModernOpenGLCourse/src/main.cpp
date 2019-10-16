9c9e340c2e3645754bdb92de99321edfb1df6a0c
dragan.djuric <dragan.djuric@bravosystems.com>
2019-10-16 15:32:42 +0200

    Added shader logic and draw calls

--- ModernOpenGLCourse/ModernOpenGLCourse/src/main.cpp
+++ ModernOpenGLCourse/ModernOpenGLCourse/src/main.cpp
@@ -8,6 +8,123 @@
 const GLint WIDTH = 800;
 const GLint HEIGHT = 600;
 
+GLuint VAO, VBO, shader;
+
+// Vertex shader
+static const char* vShader = "									\n\
+#version 330													\n\
+																\n\
+layout (location = 0) in vec3 pos;								\n\
+																\n\
+void main()														\n\
+{																\n\
+	gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);	\n\
+}";
+
+// Fragment shader
+static const char* fShader = "									\n\
+#version 330													\n\
+																\n\
+out vec4 color;													\n\
+																\n\
+void main()														\n\
+{																\n\
+	color = vec4(1.0, 0.0, 0.0, 1.0);							\n\
+}";
+
+void CreateTriangle()
+{
+	GLfloat vertices[] = {
+		-1.0f, -1.0f, 0.0f,
+		1.0f, -1.0f, 0.0f,
+		0.0f, 1.0f, 0.0f
+	};
+
+	// Create vertex array in Graphics RAM and store its ID in VAO
+	glGenVertexArrays(1, &VAO);
+	// Bind (use) vertex array with ID stored in VAO
+	glBindVertexArray(VAO);
+
+	// Generate vertex buffer and store its ID in VBO 
+	glGenBuffers(1, &VBO);
+	// Bind that buffer as array buffer
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	// Bind data from vertices to vertex array
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	// Unbind vertex buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	// Unbind vertex array
+	glBindVertexArray(0);
+}
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
+{
+	GLuint theShader = glCreateShader(shaderType);
+
+	const GLchar* theCode[1];
+	theCode[0] = shaderCode;
+
+	GLint codeLength[1];
+	codeLength[0] = strlen(shaderCode);
+
+	glShaderSource(theShader, 1, theCode, codeLength);
+	glCompileShader(theShader);
+
+	int result;
+	GLchar eLog[1024] = { 0 };
+	glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+
+	if (!result)
+	{
+		glGetProgramInfoLog(theShader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error compiling the "<< shaderType << " shader: " << eLog << std::endl;
+		return;
+	}
+
+	glAttachShader(theProgram, theShader);
+}
+
+void CompileShaders()
+{
+	shader = glCreateProgram();
+
+	if (!shader)
+	{
+		std::cout << "Error creating shader program!" << std::endl;
+		return;
+	}
+
+	AddShader(shader, vShader, GL_VERTEX_SHADER);
+	AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+	GLint result = 0;
+	GLchar eLog[1024] = { 0 };
+
+	glLinkProgram(shader);
+	glGetProgramiv(shader, GL_LINK_STATUS, &result);
+
+	if (!result)
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error linking program: " << eLog << std::endl;
+		return;
+	}
+
+	glValidateProgram(shader);
+	glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+
+	if (!result)
+	{
+		glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+		std::cout << "Error validating program: " << eLog << std::endl;
+		return;
+	}
+}
+
 int main()
 {
 	// Initialize GLFW
@@ -58,6 +175,9 @@ int main()
 	// Setup viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
 
+	CreateTriangle();
+	CompileShaders();
+
 	// Loop until window is closed
 	while (!glfwWindowShouldClose(mainWindow))
 	{
@@ -67,6 +187,14 @@ int main()
 		// Clear window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader);
+		glBindVertexArray(VAO);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(0);
+		glUseProgram(0);
 
 		// Swap to buffer that we have drawn to
 		glfwSwapBuffers(mainWindow);
