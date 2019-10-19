374a70e5864650fa5dbd1f4e3e6f08eb4b7ed9f7
DraganDjuric94 <juhuudd@gmail.com>
2019-10-19 15:18:17 +0200

    Added user input handlers for keyboard and mouse

--- ModernOpenGLCourse/ModernOpenGLCourse/src/Window.cpp
+++ ModernOpenGLCourse/ModernOpenGLCourse/src/Window.cpp
@@ -1,12 +1,62 @@
 #include "Window.h"
 
+void Window::HandleKeys(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+	
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+		return;
+	}
+
+	if (key >= 0 && key <= 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			theWindow->keys[key] = true;
+			return;
+		}
+
+		if (action == GLFW_RELEASE)
+		{
+			theWindow->keys[key] = false;
+			return;
+		}
+	}
+}
+
+void Window::HandleMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (theWindow->mouseFirstMoved)
+	{
+		theWindow->lastX = xPos;
+		theWindow->lastY = yPos;
+		theWindow->mouseFirstMoved = false;
+	}
+
+	theWindow->xChange = xPos - theWindow->lastX;
+	theWindow->yChange = theWindow->lastY - yPos; // Remove inverted camera movement
+
+	theWindow->lastX = xPos;
+	theWindow->lastY = yPos;
+}
+
+void Window::CreateCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, HandleKeys);
+	glfwSetCursorPosCallback(mainWindow, HandleMouse);
+}
+
 Window::Window()
-	: width(800), height(600), mainWindow(nullptr), bufferHeight(0), bufferWidth(0)
+	: width(800), height(600), mainWindow(nullptr), bufferHeight(0), bufferWidth(0), keys{ false }
 {
 }
 
 Window::Window(GLint windowWidth, GLint windowHeight)
-	: width(windowWidth), height(windowHeight), mainWindow(nullptr), bufferHeight(0), bufferWidth(0)
+	: width(windowWidth), height(windowHeight), mainWindow(nullptr), bufferHeight(0), bufferWidth(0), keys{ false }
 {
 }
 
@@ -50,8 +100,14 @@ int Window::Initialize()
 	// Set context for GLEW to use
 	glfwMakeContextCurrent(mainWindow);
 
+	// Handle key and mouse input
+	CreateCallbacks();
+
+	// Lock and hide mouse pointer to opened window
+	glfwSetInputMode(mainWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
 	// Allow modern extension features
-	glewExperimental = GL_TRUE;
+	glewExperimental = GL_TRUE;	
 
 	// Initialize GLEW
 	if (glewInit() != GLEW_OK)
@@ -67,4 +123,23 @@ int Window::Initialize()
 
 	// Setup viewport size
 	glViewport(0, 0, bufferWidth, bufferHeight);
+
+	// Mark this pointer as user of mainWindow
+	glfwSetWindowUserPointer(mainWindow, this);
+}
+
+GLfloat Window::GetXChange()
+{
+	GLfloat change = xChange;
+	xChange = 0;
+
+	return change;
+}
+
+GLfloat Window::GetYChange()
+{
+	GLfloat change = yChange;
+	yChange = 0;
+
+	return change;
 }
--- ModernOpenGLCourse/ModernOpenGLCourse/src/Window.h
+++ ModernOpenGLCourse/ModernOpenGLCourse/src/Window.h
@@ -11,6 +11,15 @@ private:
 	GLFWwindow* mainWindow;
 	GLint width, height;
 	GLint bufferWidth, bufferHeight;
+
+	bool keys[1024]; // Range of key characters (key pressed)
+	GLfloat lastX, lastY, xChange, yChange; // Mouse movement helpers
+	bool mouseFirstMoved;
+
+
+	static void HandleKeys(GLFWwindow* window, int key, int code, int action, int mode);
+	static void HandleMouse(GLFWwindow* window, double xPos, double yPos);
+	void CreateCallbacks();
 public:
 	Window();
 	Window(GLint windowWidth, GLint windowHeight);
@@ -22,5 +31,8 @@ public:
 	inline GLfloat GetBufferWidth() const { return bufferWidth; };
 	inline GLfloat GetBufferHeight() const { return bufferHeight; };
 	inline bool GetShouldClose() const { return glfwWindowShouldClose(mainWindow); };
+	inline bool* GetKeys() { return keys; };
+	GLfloat GetXChange();
+	GLfloat GetYChange();
 };
 
